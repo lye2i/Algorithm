fcc525dd513ab4dddbb41ecf21603f869d50dbe5
lye2i <65855978+lye2i@users.noreply.github.com>
2023-01-03 17:04:51 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(Integer.toString(i));
+            }
+        }
+        
+        sb.append(0);
+        
+        for(int i=food.length-1; i>0; i--){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(Integer.toString(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
