19cf46ff59ba74e4eb6c14c1c5a07a5bebd66add
lye2i <65855978+lye2i@users.noreply.github.com>
2023-01-13 12:09:57 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int answer[] = new int[2];
+        for(int i=1; i<=yellow; i++){
+            if(yellow%i==0 && (2*(i+yellow/i)+4)==brown){
+                    answer[0] = yellow/i+2;
+                    answer[1] = i+2;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
