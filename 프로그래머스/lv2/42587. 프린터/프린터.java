1d7d3290dc9e4b79f8524f021b1372d04ea49e46
lye2i <65855978+lye2i@users.noreply.github.com>
2023-01-18 18:36:14 +0900

    [level 2] Title: 프린터, Time: 0.53 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int n = priorities.length-1;
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i=0; i<=n; i++){
+            queue.offer(new int[]{i, priorities[i]});
+        }
+        
+        Arrays.sort(priorities);
+        
+        while(true){
+           
+            if(queue.peek()[1] == priorities[n]){
+                answer++;
+                n--;
+                if(queue.poll()[0]==location)
+                    break;
+            }
+            else{
+                queue.offer(queue.poll());
+            }
+        }
+        return answer;
+    }
+}
