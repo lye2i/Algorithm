9212faa789a194071943a90fba6bcdfccb262334
lye2i <65855978+lye2i@users.noreply.github.com>
2023-01-19 17:08:30 +0900

    [level 2] Title: 스킬트리, Time: 12.82 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,26 +1,16 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        char skillArray[] = skill.toCharArray();
         
         for(int i=0; i<skill_trees.length; i++){
-            int index = 0;
-            boolean flag = true;
+            String s = skill_trees[i].replaceAll("[^"+skill+"]", "");
+            int j=0;
             
-            for(int j=0; j<skill_trees[i].length(); j++){
-                for(int k=index; k<skillArray.length; k++){
-                    if(skill_trees[i].charAt(j) == skillArray[k]){
-                        if(k == index)  index++;
-                        else    flag = false;
-                        
-                        break;
-                    }
-                }
-                
-                if(!flag)   break;
+            for(; j<s.length(); j++){
+                if(skill.charAt(j) != s.charAt(j))  break;
             }
             
-            if(flag)    answer++;
+            if(j==s.length()) answer++;
         }
         
         return answer;
