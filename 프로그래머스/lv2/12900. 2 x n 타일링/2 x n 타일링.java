72cfc305f5f95f52d776c2db5f4dcf5400349320
lye2i <65855978+lye2i@users.noreply.github.com>
2023-01-19 22:54:10 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.91 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int[] tile = new int[n+1];
+        tile[1] = 1;
+        tile[2] = 2;
+        for(int i=3; i<=n; i++){
+            tile[i] = (tile[i-1]+tile[i-2])%1000000007;
+        }
+        return tile[n];
+    }
+}
