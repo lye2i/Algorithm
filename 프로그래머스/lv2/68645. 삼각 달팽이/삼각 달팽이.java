835a4d521c91d8e429ad6e7cce4d6b65cd0be3ef
lye2i <65855978+lye2i@users.noreply.github.com>
2023-01-22 22:55:26 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.46 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n*(n+1))/2];
+        int[][] map = new int[n][n];        
+        int x = -1, y = 0, k = 1, idx = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(i%3 == 0)
+                    x++;
+                else if(i%3 == 1)
+                    y++;
+                else{
+                    x--;
+                    y--;
+                }
+                map[x][y] = k++;
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map[i][j] == 0)  break;
+                answer[idx++] = map[i][j];
+            }
+        }
+        
+        return answer;
+    }
+}
