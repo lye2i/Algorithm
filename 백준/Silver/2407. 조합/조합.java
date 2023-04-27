6e613005aba2098b7a06662812c8498d34637682
lye2i <65855978+lye2i@users.noreply.github.com>
2023-04-27 22:30:54 +0900

    [Silver III] Title: 조합, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		BigInteger n1 = BigInteger.ONE;
+		BigInteger n2 = BigInteger.ONE;
+		
+		for(int i=0; i<m; i++) {
+			n1 = n1.multiply(new BigInteger(String.valueOf(n-i)));
+			n2 = n2.multiply(new BigInteger(String.valueOf(i+1)));
+		}
+		
+		System.out.print(n1.divide(n2));
+	}
+}
