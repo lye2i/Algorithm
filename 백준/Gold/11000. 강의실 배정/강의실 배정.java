5174e27e0ccfc3a341882255432c444fd2d9e99f
lye2i <65855978+lye2i@users.noreply.github.com>
2023-05-18 16:12:03 +0900

    [Gold V] Title: 강의실 배정, Time: 784 ms, Memory: 76460 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -8,17 +9,19 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PriorityQueue<Integer> room = new PriorityQueue<Integer>();
-		PriorityQueue<int[]> lesson = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
 		int N = Integer.parseInt(br.readLine());
+		int lesson[][] = new int[N][2];
 		int answer = 0;
 		
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			lesson.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+			lesson[i][0] = Integer.parseInt(st.nextToken());
+			lesson[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i=0; i<N; i++) {
-			int l[] = lesson.poll();
+		Arrays.sort(lesson, (a, b) -> a[0] - b[0]);
+		
+		for(int[] l : lesson) {
 			while(!room.isEmpty() && room.peek() <= l[0]) {
 				room.poll();
 			}
