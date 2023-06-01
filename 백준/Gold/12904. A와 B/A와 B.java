e417e84d74d707028f9b049267c47879d2f4d220
lye2i <65855978+lye2i@users.noreply.github.com>
2023-06-01 10:25:26 +0900

    [Gold V] Title: A와 B, Time: 104 ms, Memory: 13352 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static String A;
+	static int answer;
+	
+	static void change(StringBuilder B, int length) {
+		if(length == A.length()) {
+			if(A.equals(B.toString()))	answer = 1;
+			return;
+		}
+		
+		if(B.charAt(length-1) == 'A') {
+			StringBuilder nsb = new StringBuilder(B);
+			change(nsb.deleteCharAt(length-1), length-1);
+		}
+		
+		if(B.charAt(length-1) == 'B') {
+			StringBuilder nsb = new StringBuilder(B);
+			change(nsb.deleteCharAt(length-1).reverse(), length-1);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		A = br.readLine();
+		StringBuilder B = new StringBuilder(br.readLine());
+		
+		change(B, B.length());
+		System.out.print(answer);
+	}
+}
