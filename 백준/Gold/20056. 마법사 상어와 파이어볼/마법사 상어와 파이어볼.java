85764a69a014b8105ecb58affdb2067b8dc38820
lye2i <65855978+lye2i@users.noreply.github.com>
2023-06-05 17:00:39 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 332 ms, Memory: 31916 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M, K, idx;
 	static int R[] = {-1, -1, 0, 1, 1, 1, 0, -1}, C[] = {0, 1, 1, 1, 0, -1, -1, -1};
-	static HashMap<Integer, Fire> fire;
-	static ArrayList<Integer>[][] map;
+	static Queue<Fire> fire;
+	static Queue<Fire>[][] map;
 	
 	static class Fire {
 		int r, c, m, s, d;
@@ -23,40 +23,41 @@ public class Main {
 	}
 	
 	static void init() {
-		map = new ArrayList[N][N];
+		fire = new LinkedList<Fire>();
+		map = new LinkedList[N][N];
+		
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				map[i][j] = new ArrayList<Integer>();
+				map[i][j] = new LinkedList<Fire>();
 			}
 		}
 	}
 	
 	static void move() {
-		for(int i : fire.keySet()) {
-			Fire f = fire.get(i);
+		while(!fire.isEmpty()) {
+			Fire f = fire.poll();
 			f.r = (f.r + R[f.d]*f.s + N*f.s) % N;
 			f.c = (f.c + C[f.d]*f.s + N*f.s) % N;
 			
-			map[f.r][f.c].add(i);
-			fire.put(i, f);
+			map[f.r][f.c].add(f);
 		}
 	}
 	
 	static void combine() {
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				if(map[i][j].size() < 2)	continue;
+				if(map[i][j].size() == 1)	fire.add(map[i][j].poll());
 				
-				int max = 0, sum = 0, dir = 0, size = map[i][j].size();
-				for(int k : map[i][j]) {
-					Fire f = fire.get(k);
-					max += f.m;
-					dir += f.d%2;
-					sum += f.s;
+				else if(map[i][j].size() > 1) {
+					int max = 0, sum = 0, dir = 0, size = map[i][j].size();
+					while(!map[i][j].isEmpty()) {
+						Fire f = map[i][j].poll();
+						max += f.m;
+						dir += f.d%2;
+						sum += f.s;
+					}
+					if(max/5 > 0)	divide(i, j, max/5, dir%size, sum/size);
 				}
-				
-				remove(map[i][j]);
-				if(max/5 > 0)	divide(i, j, max/5, dir%size, sum/size);
 			}
 		}
 	}
@@ -65,14 +66,8 @@ public class Main {
 		if(d == 0)	d = 0;
 		else	d = 1;
 		
-		for(int i=d; i<8; i+=2) {
-			fire.put(idx++, new Fire(r, c, m, s, i));
-		}
-	}
-	
-	static void remove(ArrayList<Integer> list) {
-		for(int i : list) {
-			fire.remove(i);
+		for(; d<8; d+=2) {
+			fire.add(new Fire(r, c, m, s, d));
 		}
 	}
 	
@@ -82,22 +77,21 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		fire = new HashMap<Integer, Fire>();
+		init();
 		
 		while(M-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			fire.put(idx++, new Fire(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+			fire.add(new Fire(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 		
 		while(K-- > 0) {
-			init();
 			move();
 			combine();
 		}
 		
 		int ans = 0;
-		for(int i : fire.keySet()) {
-			ans += fire.get(i).m;
+		while(!fire.isEmpty()) {
+			ans += fire.poll().m;
 		}
 		System.out.print(ans);
 	}
