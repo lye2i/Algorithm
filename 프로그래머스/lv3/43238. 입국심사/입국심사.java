d3832881a2552615842474403c4e3a04e9e06abb
lye2i <65855978+lye2i@users.noreply.github.com>
2023-06-08 12:58:00 +0900

    [level 3] Title: 입국심사, Time: 0.42 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long answer = 0, left = 0, right = times[times.length-1]*(long)n;
+        
+        while(left <= right) {
+            long mid = (right + left) / 2;
+            long cnt = 0;
+            for(int t : times) {
+                cnt += mid / t;
+            }
+            
+            if(cnt < n)    left = mid + 1;
+            else {
+                right = mid - 1;
+                answer = mid;
+            }
+        }
+        
+        return answer;
+    }
+}
