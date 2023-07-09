14a65d2d6749af17465afa12d48b0e5c12aefae3
lye2i <65855978+lye2i@users.noreply.github.com>
2023-07-09 14:02:54 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(int i=0; i<name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i=0; i<photo.length; i++) {
+            for(String p : photo[i]) {
+                if(map.containsKey(p))  answer[i] += map.get(p);
+            }
+        }
+        
+        return answer;
+    }
+}
