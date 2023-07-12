628e73b81fd2396224f71772071a98572983eba5
lye2i <65855978+lye2i@users.noreply.github.com>
2023-07-12 19:25:29 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 124 ms, Memory: 13012 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		PriorityQueue<Long> pq = new PriorityQueue<Long>();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			pq.add(Long.parseLong(st.nextToken()));
+		}
+		
+		while(M-- > 0) {
+			long a = pq.poll() + pq.poll();
+			pq.add(a);
+			pq.add(a);
+		}
+		
+		long answer = 0;
+		while(!pq.isEmpty()) {
+			answer += pq.poll();
+		}
+		
+		System.out.print(answer);
+	}
+}
