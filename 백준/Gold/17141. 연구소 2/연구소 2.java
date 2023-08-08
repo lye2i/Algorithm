66215859e57745ca4249cc25d7403a0e17ac7c49
lye2i <65855978+lye2i@users.noreply.github.com>
2023-08-08 10:30:02 +0900

    [Gold IV] Title: 연구소 2, Time: 260 ms, Memory: 40484 KB -BaekjoonHub

---
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final int r[] = {-1,0,1,0}, c[] = {0,1,0,-1};
+	static int N, M, A[][], T;
+	static ArrayList<Point> virus, blank;
+	static boolean visit[][];
+	static Queue<Point> queue;
+	
+	static class Point {
+		int x, y, t;
+		Point(int x, int y, int t) {
+			this.x = x;
+			this.y = y;
+			this.t = t;
+		}
+	}
+	
+	static void combination(int idx, int cnt, boolean select[]) {
+		if(cnt == M) {
+			selectVirus(select);
+			int time = spreadVirus();
+			if(checkVirus())	T = Math.min(time, T);
+			return;
+		}
+		
+		for(int i=idx; i<virus.size(); i++) {
+			if(!select[i]) {
+				select[i] = true;
+				combination(i, cnt+1, select);
+				select[i] = false;
+			}
+		}
+	}
+	
+	static void selectVirus(boolean select[]) {
+		visit = new boolean[N][N];
+		
+		for(int i=0; i<virus.size(); i++) {
+			if(select[i]) {
+				Point v = virus.get(i);
+				queue.add(v);
+				visit[v.x][v.y] = true;
+			}
+		}
+	}
+	
+	static int spreadVirus() {
+		int time = 0;
+		
+		while(!queue.isEmpty()) {
+			Point p = queue.poll();
+			
+			for(int d=0; d<4; d++) {
+				int dr = p.x + r[d];
+				int dc = p.y + c[d];
+				
+				if(dr < 0 || dr >= N || dc < 0 || dc >= N || visit[dr][dc] || A[dr][dc] == 1)	continue;
+				visit[dr][dc] = true;
+				queue.add(new Point(dr, dc, p.t+1));
+				time = Math.max(time, p.t+1);
+			}
+		}
+		
+		return time;
+	}
+	
+	static boolean checkVirus() {
+		for(Point p : blank) {
+			if(!visit[p.x][p.y])	return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		A = new int[N][N];
+		T = Integer.MAX_VALUE;
+		virus = new ArrayList<Point>();
+		blank = new ArrayList<Point>();
+		queue = new LinkedList<Point>();
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
+				
+				if(A[i][j] != 1)	blank.add(new Point(i, j, 0));
+				if(A[i][j] == 2)	virus.add(new Point(i, j, 0));
+			}
+		}
+		
+		combination(0, 0, new boolean[virus.size()]);
+		
+		System.out.print(T == Integer.MAX_VALUE ? -1 : T);
+	}
+}
