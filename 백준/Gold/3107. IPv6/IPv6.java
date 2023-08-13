c27c23ae6c1639e8e0649faf2294fd847ff9dafa
lye2i <65855978+lye2i@users.noreply.github.com>
2023-08-13 21:11:11 +0900

    [Gold V] Title: IPv6, Time: 136 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Gold/3107. IPv6/IPv6.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String addr[] = br.readLine().split(":", -1);
+		
+		for(int i=0, idx = 0; i<addr.length; i++) {
+			if(addr[i].length() == 0) {
+				while(8 - idx >= addr.length - i) {
+					sb.append("0000:");
+					idx++;
+				}
+				continue;
+			}
+			
+			for(int j=4; j>addr[i].length(); j--) {
+				sb.append('0');
+			}
+			sb.append(addr[i]).append(':');
+			idx++;
+		}
+		
+		sb.deleteCharAt(sb.length()-1);
+		System.out.print(sb);
+	}
+}
