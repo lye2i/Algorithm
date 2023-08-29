0d81c153d2f6981b2b460494c8c69694df27fd77
lye2i <65855978+lye2i@users.noreply.github.com>
2023-08-29 18:30:27 +0900

    [Gold IV] Title: 불!, Time: 496 ms, Memory: 70148 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final int r[] = {-1,0,1,0}, c[] = {0,1,0,-1};
+	static int R, C;
+	static char map[][];
+	static Queue<Point> person, fire;
+	static boolean visitFire[][], visitPerson[][];
+	
+	static class Point {
+		int x, y;
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static void getPoint() {
+		for(int i=0; i<R; i++) {
+			for(int j=0; j<C; j++) {
+				if(map[i][j] == 'J') {
+					visitPerson[i][j] = true;
+					person.add(new Point(i, j));
+				} else if(map[i][j] == 'F') {
+					visitFire[i][j] = true;
+					fire.add(new Point(i, j));
+				}
+			}
+		}
+	}
+	
+	static void spreadFire() {
+		int n = fire.size();
+		while(n-- > 0) {
+			Point p = fire.poll();
+			
+			for(int d=0; d<4; d++) {
+				int dr = p.x + r[d];
+				int dc = p.y + c[d];
+				
+				if(dr < 0 || dr >= R || dc < 0 || dc >= C || visitFire[dr][dc] || map[dr][dc] == '#')	continue;
+				
+				visitFire[dr][dc] = true;
+				map[dr][dc] = 'F';
+				fire.add(new Point(dr, dc));
+			}
+		}
+	}
+	
+	static boolean movePerson() {
+		int n = person.size();
+		while(n-- > 0) {
+			Point p = person.poll();
+			
+			for(int d=0; d<4; d++) {
+				int dr = p.x + r[d];
+				int dc = p.y + c[d];
+				
+				if(dr < 0 || dr >= R || dc < 0 || dc >= C)	return true;
+				if(visitPerson[dr][dc] || map[dr][dc] != '.')	continue;
+				
+				visitPerson[dr][dc] = true;
+				person.add(new Point(dr, dc));
+			}
+		}
+		
+		return false;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		person = new LinkedList<Point>();
+		fire = new LinkedList<Point>();
+		visitFire = new boolean[R][C];
+		visitPerson = new boolean[R][C];
+		
+		for(int i=0; i<R; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		
+		getPoint();
+		
+		int cnt = 0;
+		while(++cnt > 0) {
+			if(person.size() == 0) {
+				cnt = 0;
+				break;
+			}
+			
+			spreadFire();
+			if(movePerson())	break;
+		}
+		
+		System.out.print(cnt == 0 ? "IMPOSSIBLE" : cnt);
+	}
+}
