de0784fa9c50b2fc4fac173414a5ac1579fe95f5
lye2i <65855978+lye2i@users.noreply.github.com>
2023-08-30 15:33:36 +0900

    [Silver II] Title: 예산, Time: 172 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/README.md
+++ 백준/Silver/2512. 예산/README.md
@@ -1,14 +1,14 @@
-# [Silver III] 예산 - 2512 
+# [Silver II] 예산 - 2512 
 
 [문제 링크](https://www.acmicpc.net/problem/2512) 
 
 ### 성능 요약
 
-메모리: 13948 KB, 시간: 112 ms
+메모리: 15672 KB, 시간: 172 ms
 
 ### 분류
 
-이분 탐색(binary_search), 매개 변수 탐색(parametric_search)
+이분 탐색, 매개 변수 탐색
 
 ### 문제 설명
 
--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,40 +1,37 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int money[] = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for(int i=0; i<N; i++)
+		for(int i=0; i<N; i++) {
 			money[i] = Integer.parseInt(st.nextToken());
+		}
+		int total = Integer.parseInt(br.readLine());
+		int max = 0, sum = 0, n = N;
 		
-		int max = Integer.parseInt(br.readLine());
 		Arrays.sort(money);
-		int min = 0;
-		int n = N;
-		int sum = 0;
-		int answer = 0;
 		
-		while(n > 0) {
-			if(sum + money[min]*n > max) {
-				answer = money[min];
+		for(int i=0; i<N; i++, n--) {
+			if(sum + money[i]*n > total) {
+				max = money[i];
 				break;
 			}
-			sum += money[min++];
-			n--;
+			
+			sum += money[i];
 		}
 		
 		while(n > 0) {
-			if(sum + answer*n <= max)	break;
-			answer--;
+			if(sum + max * n <= total)	break;
+			max--;
 		}
 		
-		System.out.print(answer == 0 ? money[N-1] : answer);
+		System.out.print(max == 0 ? money[N-1] : max);
 	}
-
 }
