7964dfbfeb6c0d4922bc8930ebe655440ddf6512
lye2i <65855978+lye2i@users.noreply.github.com>
2023-10-06 15:18:41 +0900

    [Gold II] Title: 컬러볼, Time: 1012 ms, Memory: 81076 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.java
+++ 백준/Gold/10800. 컬러볼/컬러볼.java
@@ -1,55 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-class Ball implements Comparable<Ball>{
-	int idx, color, size;
-	
-	Ball(int idx, int color, int size) {
-		this.idx = idx;
-		this.color = color;
-		this.size = size;
-	}
-	
-	@Override
-	public int compareTo(Ball b) {
-		return this.size - b.size;
-	}
-}
-
 public class Main {
+	
+	static class Ball implements Comparable<Ball>{
+		int n, c, s;
+		
+		Ball(int n, int c, int s) {
+			this.n = n;
+			this.c = c;
+			this.s = s;
+		}
+		
+		@Override
+		public int compareTo(Ball b) {
+			return this.s - b.s;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		int C[] = new int[N];
-		int A[] = new int[N];
-		int sum = 0;
-		Ball ball[] = new Ball[N];
+		ArrayList<Ball> list = new ArrayList<Ball>();
+		int sum[] = new int[N+1];
+		int answer[] = new int[N];
 		
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			ball[i] = new Ball(i, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken()));
+			list.add(new Ball(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 		
-		Arrays.sort(ball);
+		Collections.sort(list);
 		
 		for(int i=0, j=0; i<N; i++) {
-			Ball b = ball[i];
-			while(ball[j].size < b.size) {
-				sum += ball[j].size;
-				C[ball[j].color] += ball[j].size;
-				j++;
+			Ball ball = list.get(i);
+			
+			while(list.get(j).s < ball.s) {
+				Ball next = list.get(j++);
+				sum[0] += next.s;
+				sum[next.c] += next.s;
 			}
-			A[b.idx] = sum - C[b.color];
-		}
-
-		for(int a : A) {
-			sb.append(a).append("\n");
+			
+			answer[ball.n] = sum[0] - sum[ball.c];
 		}
 		
+		for(int ans : answer) {
+			sb.append(ans).append('\n');
+		}
 		System.out.print(sb);
 	}
 }
