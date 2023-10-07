2aa9c032e50f98004ff8fefcd1cf49dd188fa572
lye2i <65855978+lye2i@users.noreply.github.com>
2023-10-08 01:20:35 +0900

    [level 3] Title: 여행경로, Time: 13.61 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/README.md
+++ 프로그래머스/lv3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 0.49 ms
+메모리: 82.3 MB, 시간: 13.61 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/lv3/43164. 여행경로/여행경로.java
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -4,36 +4,38 @@ class Solution {
     static int N;
     static String answer[];
     
-    public static boolean search(int idx, boolean visit[], String[][] tickets, String city) {
-        if(idx == N) {
-            answer[idx] = city;
-            return true;
-        }
+    public boolean DFS(int idx, String[][] tickets, boolean visit[]) {
+        if(idx == N)    return true;
         
         for(int i=0; i<N; i++) {
-            if(!visit[i] && tickets[i][0].equals(city)) {
+            if(!visit[i] && tickets[i][0].equals(answer[idx])) {
                 visit[i] = true;
-                answer[idx] = city;
-                if(search(idx+1, visit, tickets, tickets[i][1]))    return true;
+                answer[idx+1] = tickets[i][1];
+                if(DFS(idx+1, tickets, visit))  return true;
                 visit[i] = false;
             }
         }
         
         return false;
-    }
+    } 
     
     public String[] solution(String[][] tickets) {
         N = tickets.length;
         answer = new String[N+1];
         
-        Arrays.sort(tickets, new Comparator<String[]>() {
+        Arrays.sort(tickets, new Comparator<String[]>(){
             @Override
             public int compare(String a[], String b[]) {
                 return a[1].compareTo(b[1]);
             }
         });
         
-        search(0, new boolean[N], tickets, "ICN");
+        for(String str[] : tickets) {
+            System.out.println(str[0]+" "+str[1]);
+        }
+        
+        answer[0] = "ICN";
+        DFS(0, tickets, new boolean[N]);
         
         return answer;
     }
