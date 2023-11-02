267840f7c599bec881ca855d2fe2a949955a4f9e
lye2i <65855978+lye2i@users.noreply.github.com>
2023-11-02 09:09:10 +0900

    [Silver I] Title: 스티커, Time: 768 ms, Memory: 104436 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int sticker[][] = new int[2][N+1];
+			sticker[0][0] = 0; sticker[1][0] = 0;
+			
+			for(int i=0; i<2; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j=1; j<=N; j++) {
+					sticker[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int i=2; i<=N; i++) {
+				sticker[0][i] = Math.max(sticker[1][i-1], Math.max(sticker[0][i-2], sticker[1][i-2])) + sticker[0][i];
+				sticker[1][i] = Math.max(sticker[0][i-1], Math.max(sticker[0][i-2], sticker[1][i-2])) + sticker[1][i];
+			}
+			
+			sb.append(Math.max(sticker[0][N], sticker[1][N])).append('\n');
+		}
+		
+		System.out.print(sb);
+	}
+}
