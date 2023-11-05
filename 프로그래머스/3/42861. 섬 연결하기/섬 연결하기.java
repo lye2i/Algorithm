77b202323ff9876350908f6650f85e953d3b6856
lye2i <65855978+lye2i@users.noreply.github.com>
2023-11-05 13:57:45 +0900

    [level 3] Title: 섬 연결하기, Time: 0.65 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        int island[] = new int[n];
+        Arrays.sort(costs, (a, b) -> (a[2] - b[2]));
+        
+        for(int i=0; i<n; i++) {
+            island[i] = i;
+        }
+        
+        for(int i=0; i<costs.length; i++) {
+            if(find(island, costs[i][0]) != find(island, costs[i][1]))  {
+                union(island, costs[i][0], costs[i][1]);
+                answer += costs[i][2];
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int find(int island[], int n) {
+        if(island[n] == n)  return n;
+        return find(island, island[n]);
+    }
+    
+    public void union(int island[], int a, int b) {
+        int n1 = find(island, a);
+        int n2 = find(island, b);
+        island[n1] = n2;
+    }
+}
