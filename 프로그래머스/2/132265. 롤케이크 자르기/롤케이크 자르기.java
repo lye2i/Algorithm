b1f43bfd5fd055000d35ec0c7118d8f7f3cdbf3c
lye2i <65855978+lye2i@users.noreply.github.com>
2023-11-12 13:49:27 +0900

    [level 2] Title: 롤케이크 자르기, Time: 230.68 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,24 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0, cnt1 = 0, cnt2 = 0;
-        HashMap<Integer, Integer> total = new HashMap<Integer, Integer>();
-        HashMap<Integer, Integer> person = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> person1 = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> person2 = new HashMap<Integer, Integer>();
         
         for(int t : topping){
-            if(!total.containsKey(t)) {
-                total.put(t, 1);
-                cnt2++;
-            } else  total.put(t, total.get(t)+1);
+            if(!person2.containsKey(t)) cnt2++;
+            person2.put(t, person2.getOrDefault(t, 0) + 1);
         }
         
         for(int t : topping){
-            if(!person.containsKey(t)){
-                person.put(t, 1);
-                cnt1++;
-            }
+            if(!person1.containsKey(t)) cnt1++;
+            person1.put(t, person1.getOrDefault(t, 0) + 1);
             
-            if(total.get(t) == 1)  cnt2--;
-            total.put(t, total.get(t)-1);
+            if(person2.get(t) == 1)  cnt2--;
+            person2.put(t, person2.get(t)-1);
             
             if(cnt1 == cnt2)    answer++;
         }
