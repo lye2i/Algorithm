238991fd960610881d1b9400d4c7ec9956b0cdf7
lye2i <65855978+lye2i@users.noreply.github.com>
2025-01-16 22:19:02 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,37 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int H = Integer.parseInt(st.nextToken());
 		int W = Integer.parseInt(st.nextToken());
-		int ans = 0;
-		boolean visit[][] = new boolean[H][W];
+		boolean map[][] = new boolean[H][W];
+		int answer = 0;
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<W; i++) {
 			int h = Integer.parseInt(st.nextToken());
-			for(int j=H-h; j<H; j++)
-				visit[j][i] = true;
+			for(int j=H-1; j>=H-h; j--) {
+				map[j][i] = true;
+			}
 		}
 		
-		for(int i=0; i<H; i++) {
-			boolean block = false;			
+		for(int i=H-1; i>=0; i--) {
+			boolean flag = false;
 			int cnt = 0;
 			
 			for(int j=0; j<W; j++) {
-				if(visit[i][j] && !block)	block = true;
-				else if(!visit[i][j] && block)	cnt++;
-				else if(visit[i][j] && block && cnt > 0) {
-					ans += cnt;
-					cnt = 0;
+				if(map[i][j]) {
+					if(!flag)	flag = true;
+					else {
+						answer += cnt;
+						cnt = 0;
+					}
+				} else if(flag){
+					cnt++;
 				}
 			}
 		}
 		
-		System.out.print(ans);
+		System.out.print(answer);
 	}
 }
