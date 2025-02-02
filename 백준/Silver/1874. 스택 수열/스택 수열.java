70bd7eb909bc35419a479c95368ba11dfc523a09
lye2i <65855978+lye2i@users.noreply.github.com>
2025-02-02 21:06:44 +0900

    [Silver II] Title: 스택 수열, Time: 316 ms, Memory: 26792 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,37 +1,40 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		Stack<Integer> stack = new Stack<Integer>();
 		int N = Integer.parseInt(br.readLine());
 		int num[] = new int[N];
-		boolean flag = true;
+		StringBuilder sb = new StringBuilder();
+		
 		for(int i=0; i<N; i++) {
 			num[i] = Integer.parseInt(br.readLine());
 		}
 		
-		int n = 1, idx = 0;
-		while(flag && n <= N) {
-			if(n <= num[idx]) {
+		int idx = 0, n = 1;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		while(idx < N) {
+			while(n <= num[idx]) {
 				stack.push(n);
 				sb.append("+\n");
 				n++;
-			} else {
-				if(!stack.isEmpty() && stack.pop() == num[idx++])	sb.append("-\n");
-				else	flag = false;
+			}
+			
+			if(!stack.isEmpty()) {
+				if(stack.peek() != num[idx])	break;
+				else {
+					stack.pop();
+					sb.append("-\n");
+					idx++;
+				}
 			}
 		}
 		
-		while(flag && !stack.isEmpty()) {
-			if(stack.pop() == num[idx++])	sb.append("-\n");
-			else	flag = false;
-		}
-		
-		System.out.print(flag ? sb : "NO");
+		System.out.print(idx == N ? sb : "NO");
 	}
 }
