6cff9cc1cfae25d4787d96502f53374d88343e4f
lye2i <65855978+lye2i@users.noreply.github.com>
2025-02-02 17:53:22 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,65 +1,68 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int r[] = {-1,0,1,0};
-	static int c[] = {0,1,0,-1};
-	
-	public static void main(String[] args) throws Exception {
+	final static int r[] = {-1, 0, 1, 0};
+	final static int c[] = {0, 1, 0, -1};
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		int map[][] = new int[N][M];
+		
 		st = new StringTokenizer(br.readLine());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		int D = Integer.parseInt(st.nextToken());
-		int A[][] = new int[N][M];
-		int cnt = 0;
+		int sr = Integer.parseInt(st.nextToken());
+		int sc = Integer.parseInt(st.nextToken());
+		int sd = Integer.parseInt(st.nextToken());
+		int clean = 0;
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++)
-				A[i][j] = Integer.parseInt(st.nextToken());
+			for(int j=0; j<M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
 		
 		while(true) {
-			if(A[R][C] == 0) {
-				A[R][C] = 2;
-				cnt++;
+			if(map[sr][sc] == 0) {
+				map[sr][sc] = 2;
+				clean++;
 			}
 			
-			boolean flag = false;
+			boolean go = false;
 			
 			for(int d=0; d<4; d++) {
-				int dr = R + r[d];
-				int dc = C + c[d];
-				if(dr >= 0 && dr < N && dc >= 0 && dc < M && A[dr][dc] == 0)	flag = true;
+				int dr = sr + r[d];
+				int dc = sc + c[d];
+				if(dr >= 0 && dr < N && dc >= 0 && dc < M && map[dr][dc] == 0)	go = true;
 			}
 			
-			if(!flag) {
-				int dr = R + r[(D+2)%4];
-				int dc = C + c[(D+2)%4];
-				if(dr >= 0 && dr < N && dc >= 0 && dc < M && A[dr][dc] != 1) {
-					R = dr;
-					C = dc;
-				} 
-				else	break;
+			if(!go) {
+				int gr = sr + r[(sd+2)%4];
+				int gc = sc + c[(sd+2)%4];
+				if(gr < 0 || gr >= N || gc < 0 || gc >= M || map[gr][gc] == 1)	break;
+				else {
+					sr = gr;
+					sc = gc;
+				}
 			} else {
-				for(int d=0; d<4; d++) {
-					int dr = R + r[(D-d+3)%4];
-					int dc = C + c[(D-d+3)%4];
-					if(dr >= 0 && dr < N && dc >= 0 && dc < M && A[dr][dc] == 0) {
-						R = dr;
-						C = dc;
-						D = (D-d+3)%4;
+				for(int k=3; k>=0; k--) {
+					int gr = sr + r[(sd + k) % 4];
+					int gc = sc + c[(sd + k) % 4];
+					if(gr >= 0 && gr < N && gc >= 0 && gc < M && map[gr][gc] == 0) {
+						sr = gr;
+						sc = gc;
+						sd = (sd + k) % 4;
 						break;
 					}
 				}
 			}
 		}
 		
-		System.out.print(cnt);
+		System.out.print(clean);
 	}
 }
