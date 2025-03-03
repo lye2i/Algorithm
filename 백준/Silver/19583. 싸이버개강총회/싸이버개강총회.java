7efe23ff03121edd80c8a89bbd99ca99ef142bf9
lye2i <65855978+lye2i@users.noreply.github.com>
2025-03-03 10:47:31 +0900

    [Silver II] Title: 싸이버개강총회, Time: 456 ms, Memory: 54748 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -1,27 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		HashSet<String> start = new HashSet<String>();
-		HashSet<String> end = new HashSet<String>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		String S = st.nextToken(), E = st.nextToken(), Q = st.nextToken();
-		String str = br.readLine();
+		HashSet<String> in = new HashSet<String>();
+		HashSet<String> out = new HashSet<String>();
+		String str = "";
 		
-		while(str != null && str.length() > 0) {
+		while((str = br.readLine()) != null && str.length() > 0) {
 			String chat[] = str.split(" ");
-			
-			if(chat[0].compareTo(S) <= 0)	start.add(chat[1]);
-			else if(start.contains(chat[1]) && chat[0].compareTo(E) >= 0 && chat[0].compareTo(Q) <= 0)	end.add(chat[1]);
-			
-			str = br.readLine();
+			if(chat[0].compareTo(S) <= 0) {
+				in.add(chat[1]);
+			} else if(chat[0].compareTo(E) >= 0 && chat[0].compareTo(Q) <= 0 && in.contains(chat[1])) {
+				out.add(chat[1]);
+			}
 		}
 		
-		System.out.print(end.size());
+		System.out.print(out.size());
 	}
 }
