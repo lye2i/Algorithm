fbb87274144bcc3a6eba03c6f0e5c5bb0e5b9f9d
lye2i <65855978+lye2i@users.noreply.github.com>
2025-04-13 21:05:15 +0900

    [Silver I] Title: 컴백홈, Time: 128 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.java
+++ 백준/Silver/1189. 컴백홈/컴백홈.java
@@ -1,23 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static final int r[] = {-1,0,1,0}, c[] = {0,1,0,-1};
-	static int R, C, K, ans;
-	static char M[][];
+	final static int r[] = {-1, 0, 1, 0}, c[] = {0, 1, 0, -1};
+	static int R, C, K, answer;
+	static char map[][];
 	
-	static void road(int cnt, int x, int y, boolean visit[][]) {
-		if(x == 0 && y == C-1) {
-			if(cnt == K)	ans++;
+	public static void search(int x, int y, int k, boolean visit[][]) {
+		if(x == 0 && y == C-1 && k == K) {
+			answer++;
 			return;
 		}
-
+		
 		for(int d=0; d<4; d++) {
 			int dr = x + r[d];
 			int dc = y + c[d];
-			if(dr < 0 || dr >= R || dc < 0 || dc >= C || M[dr][dc] == 'T' || visit[dr][dc] || cnt >= K)	continue;
+			if(dr < 0 || dr >= R || dc < 0 || dc >= C || map[dr][dc] == 'T' || visit[dr][dc] || k + 1 > K)	continue;
 			visit[dr][dc] = true;
-			road(cnt+1, dr, dc, visit);
+			search(dr, dc, k+1, visit);
 			visit[dr][dc] = false;
 		}
 	}
@@ -28,17 +29,16 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		M = new char[R][C];
-		ans = 0;
+		map = new char[R][C];
 		
 		for(int i=0; i<R; i++) {
-			M[i] = br.readLine().toCharArray();
+			map[i] = br.readLine().toCharArray();
 		}
 		
 		boolean visit[][] = new boolean[R][C];
 		visit[R-1][0] = true;
-		road(1, R-1, 0, visit);
+		search(R-1, 0, 1, visit);
 		
-		System.out.print(ans);
+		System.out.print(answer);	
 	}
 }
