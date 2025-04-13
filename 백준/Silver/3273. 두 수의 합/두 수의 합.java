18171a07bebeb9b5791f08e8d23ae9c0a2e7fc22
lye2i <65855978+lye2i@users.noreply.github.com>
2025-04-13 21:17:14 +0900

    [Silver III] Title: 두 수의 합, Time: 316 ms, Memory: 27164 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,31 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int num[] = new int[N];
+		int arr[] = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int X = Integer.parseInt(br.readLine()), answer = 0;
+		int sum = Integer.parseInt(br.readLine());
 		
-		Arrays.sort(num);
-		int left = 0, right = N-1;
+		Arrays.sort(arr);
+		
+		int left = 0, right = N-1, cnt = 0;
+		
 		while(left < right) {
-			if(num[left] + num[right] < X)	left++;
-			else if(num[left] + num[right] > X)	right--;
-			else {
+			if(arr[left] + arr[right] == sum) {
+				cnt++;
 				left++;
-				answer++;
+			} else if(arr[left] + arr[right] < sum) {
+				left++;
+			} else {
+				right--;
 			}
 		}
 		
-		System.out.print(answer);
+		System.out.print(cnt);
 	}
 }
